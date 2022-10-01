a28cdb845cd3673990237dffb6ace85ca3234928
Lloyd Perez <lloyd4965jr@yahoo.com>
2022-09-30 22:12:53 -0400

    Completed 5.3

---
+++ src/main/java/DTOs/Error/ErrorDetail.java
@@ -0,0 +1,50 @@
+package DTOs.Error;
+
+public class ErrorDetail {
+
+    String title = "Internal Service Error";
+    int status = 404;
+    String detail = "Not Found";
+    long timeStamp = System.currentTimeMillis();
+    String developerMessage = "ResourceNotFoundException";
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getDeveloperMessage() {
+        return developerMessage;
+    }
+
+    public void setDeveloperMessage(String developerMessage) {
+        this.developerMessage = developerMessage;
+    }
+}
